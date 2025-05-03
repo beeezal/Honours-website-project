394a2cf7cf8ad0f83c42e7875032c3cd18fabec1
beeezal <beeezal@users.noreply.github.com>
2025-05-03 20:57:56 +0000

    deploy: 18f542b48beaac39832abbdf5467257d7b0ac758

---
+++ assets/scripts/chap2_02.js
@@ -0,0 +1,75 @@
+let normalWalkerScript = function(p) {
+  class WalkerNormal {
+    constructor(x, y, r) {
+      this.location = p.createVector(x, y);
+      this.velocity = p5.Vector.random2D();  
+      this.r = r;    
+      this.D = this.r * 2;                       
+      this.velAngle = this.velocity.heading();
+    }
+    
+    display() {
+      p.stroke(0);
+      p.fill(100);
+      p.strokeWeight(1);
+      p.circle(this.location.x, this.location.y, this.D);
+    }
+    
+    update(vel_mag=1, chk_edges=false, stdDev=p.PI/8) {
+      if (p.frameCount % 3 === 0) {
+        this.velAngle = p.randomGaussian() * stdDev + this.velocity.heading();
+      }
+      this.velocity.set(p.cos(this.velAngle), p.sin(this.velAngle));
+      this.velocity.mult(vel_mag);
+      this.location.add(this.velocity);
+      if (chk_edges) {this.checkEdges()};
+    }
+    
+    checkEdges() {
+      if (this.location.x > p.width) this.location.x = 0;
+      else if (this.location.x < 0) this.location.x = p.width;
+      if (this.location.y > p.height) this.location.y = 0;
+      else if (this.location.y < 0) this.location.y = p.height;
+    }
+  }
+
+  let w = new WalkerNormal(p.width / 2, p.height / 2, 10);
+  let stdDevSlider, label, container;
+
+  let stdDev = p.PI/8;
+
+  p.initializeSketch = function() {
+    w.location.set(p.width/2, p.height/2);
+    w.velocity.set(p5.Vector.random2D());
+    p.background(200);
+
+    p.redraw();
+  };
+
+  p.setup = function() {
+    p.noLoop();
+
+    let cnv = p.createCanvas(p.windowWidth, p.windowHeight);
+    cnv.parent('sec2');
+    container = document.getElementById('interactive-controls-sec2');
+
+
+    stdDevSlider = p.createSlider(p.PI/60, p.PI/6, p.PI/8, 0);
+    stdDevSlider.parent(container);
+    label = p.createSpan('Std Dev: ' + stdDevSlider.value().toFixed(3));
+    label.parent(container);
+
+    stdDevSlider.input(() => {
+      stdDevLabel.html('Std Dev: ' + p.degrees(stdDevSlider.value()) + '°');
+      stdDev = stdDevSlider.value();
+    });
+  };
+
+  p.draw = function() {
+    w.display();
+    w.update(3, true, stdDev);
+  };
+
+};
+
+let sec2Sketch = new p5(normalWalkerScript, 'sec2');
---
+++ assets/scripts/chap2_04.js
@@ -0,0 +1,87 @@
+let rWalkerScript = function(p) {
+  class rWalker {
+    constructor(x, y, r) {
+      this.location = p.createVector(x, y);
+      this.v = p5.Vector.random2D();
+      this.r = r;
+      this.sa = p.random(0, p.TWO_PI);
+      this.life = 10 * 60;
+    } 
+
+    step(step_size = 1, t_inteval = 1) {
+      if (this.life % t_inteval == 0) {
+        this.sa = p.random(0, p.TWO_PI);
+      }
+      this.v.set(p.cos(this.sa), p.sin(this.sa));
+      this.v.mult(step_size);
+      this.location.add(this.v);
+    }
+
+    display() {
+      p.stroke(0);
+      p.fill(100);
+      p.circle(this.location.x, this.location.y, this.r);
+      this.life -= 1;
+    }
+
+    isDead() {
+      return (this.life <= 0);
+    }
+  }
+
+  class rWalkerSystem {
+    constructor() {
+      this.walkers = [];
+    }
+
+    addWalker(x, y, r) {
+      this.walkers.push(new rWalker(x, y, r));
+    }
+
+    run() {
+      let length = this.walkers.length - 1;
+      for (let i = length; i >= 0; i--) {
+        this.walkers[i].step(3, 15);
+        this.walkers[i].display();
+        if (this.walkers[i].isDead()) {
+          this.walkers.splice(i, 1);
+        }
+      }
+    } 
+  }
+
+  let rWalkers = new rWalkerSystem();
+
+  p.initializeSketch = function() {
+    for (let i = 0; i < 5; i++) {
+      rWalkers.addWalker(
+        p.random(p.width/3, 2*p.width/3), 
+        p.random(p.height/3, 2*p.height/3), 
+        15
+      );
+    }
+    
+    p.redraw();
+  };
+
+  p.setup = function() {
+    p.noLoop();
+
+    let cnv = p.createCanvas(p.windowWidth, p.windowHeight);
+    cnv.parent('sec4');
+    
+    p.initializeSketch();
+  };
+
+  p.draw = function() {
+    p.background(200, 40);
+    rWalkers.run();
+  };
+
+  p.mouseDragged = function() {
+    rWalkers.addWalker(p.mouseX, p.mouseY, 15);
+    p.redraw();
+  };
+};
+
+let sec4Sketch = new p5(rWalkerScript, 'sec4');

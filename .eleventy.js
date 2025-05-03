bbecaa039508c8c39f31023c4702bfecd6360838
Bezal Stephen <bezal.stephen22ug@apu.edu.in>
2025-05-03 14:03:21 +0530

    chore: add updates made at time of last commit
    - include plugins for markdown rendering - superscript and math
    - create an empty table of contents file
    - remove test1.md

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,18 @@
+import mathjaxPlugin from 'eleventy-plugin-mathjax';
+import markdownItSup from  'markdown-it-sup';
+
 export default function(eleventyConfig) {
   // Pass through files
   eleventyConfig.addPassthroughCopy('src/assets');
 
   // Layouts alias
   eleventyConfig.addLayoutAlias('base', 'base.html');
+
+    // Add Markdown-it plugins
+    eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItSup));
+    
+    // Add MathJax plugin
+    eleventyConfig.addPlugin(mathjaxPlugin);
 
   // base config
   return {
